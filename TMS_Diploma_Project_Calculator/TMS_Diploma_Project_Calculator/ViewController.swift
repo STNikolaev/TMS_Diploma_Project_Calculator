e5ef5bb5b2b9d8f7b91ac5ccb5bc53d53ab9af7d
STNikolaev <a.o.nikolaev@icloud.com>
2022-04-19 14:14:52 +0300

    Initial commit

---
+++ TMS_Diploma_Project_Calculator/TMS_Diploma_Project_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMS_Diploma_Project_Calculator
+//
+//  Created by Alexandr Nikolaev on 19.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
